c14a2b5168d74a171fc93deb38b831553cc4dc40
caglapiskin <454523@ogr.ktu.edu.tr>
2026-06-14 13:35:13 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FirinMatikPro
+namespace FirinMatikPro
 {
     partial class Form1
     {
@@ -28,6 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -44,95 +49,151 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 52);
+            label1.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.Location = new Point(18, 94);
             label1.Name = "label1";
-            label1.Size = new Size(70, 20);
+            label1.Size = new Size(98, 25);
             label1.TabIndex = 0;
-            label1.Text = "Ürün Adı:";
+            label1.Text = "Ürün Adı :";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 72);
+            label2.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label2.Location = new Point(18, 118);
             label2.Name = "label2";
-            label2.Size = new Size(76, 20);
+            label2.Size = new Size(102, 25);
             label2.TabIndex = 1;
             label2.Text = "Fiyatı (TL):";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 92);
+            label3.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label3.Location = new Point(18, 141);
             label3.Name = "label3";
-            label3.Size = new Size(59, 20);
+            label3.Size = new Size(79, 25);
             label3.TabIndex = 2;
             label3.Text = "Barkod:";
             // 
             // button1
             // 
-            button1.Location = new Point(98, 159);
+            button1.BackColor = Color.Sienna;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(18, 191);
             button1.Name = "button1";
-            button1.Size = new Size(189, 29);
+            button1.Size = new Size(193, 29);
             button1.TabIndex = 3;
             button1.Text = "Ürünü Fırına Kaydet";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // txtBarkod
             // 
-            txtBarkod.Location = new Point(106, 92);
+            txtBarkod.Location = new Point(140, 142);
             txtBarkod.Name = "txtBarkod";
-            txtBarkod.Size = new Size(125, 27);
+            txtBarkod.Size = new Size(153, 27);
             txtBarkod.TabIndex = 4;
             // 
             // txtFiy
             // 
-            txtFiy.Location = new Point(106, 69);
+            txtFiy.Location = new Point(140, 119);
             txtFiy.Name = "txtFiy";
-            txtFiy.Size = new Size(125, 27);
+            txtFiy.Size = new Size(153, 27);
             txtFiy.TabIndex = 5;
             // 
             // txtUrunAdi
             // 
-            txtUrunAdi.Location = new Point(106, 45);
+            txtUrunAdi.Location = new Point(140, 92);
             txtUrunAdi.Name = "txtUrunAdi";
-            txtUrunAdi.Size = new Size(125, 27);
+            txtUrunAdi.Size = new Size(153, 27);
             txtUrunAdi.TabIndex = 6;
+            txtUrunAdi.TextChanged += txtUrunAdi_TextChanged;
             // 
             // dataGridView1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 194);
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(253, 251, 247);
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.Sienna;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 10.5F, FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            
+            // --- MAVİ RENK KALINTILARI BURADA TAMAMEN SIYAH KAFFESİNE ÇEVRİLDİ ---
+            dataGridViewCellStyle2.SelectionBackColor = Color.Sienna;
+            dataGridViewCellStyle2.SelectionForeColor = Color.White;
+            
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.ColumnHeadersHeight = 40;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Window;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 10F);
+            dataGridViewCellStyle3.ForeColor = Color.FromArgb(64, 64, 64);
+            dataGridViewCellStyle3.SelectionBackColor = Color.Sienna;
+            dataGridViewCellStyle3.SelectionForeColor = Color.White;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = Color.FromArgb(240, 240, 240);
+            dataGridView1.Location = new Point(18, 226);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 51;
+            
+            // Köşe hücrelerin seçim rengini de Sienna yapıyoruz
+            dataGridView1.RowHeadersDefaultCellStyle.SelectionBackColor = Color.Sienna;
+            
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.RowTemplate.Height = 32;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.Size = new Size(640, 222);
             dataGridView1.TabIndex = 7;
-            
             // 
             // button2
             // 
-            button2.Location = new Point(293, 159);
+            button2.BackColor = Color.Sienna;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(238, 191);
             button2.Name = "button2";
-            button2.Size = new Size(137, 29);
+            button2.Size = new Size(193, 29);
             button2.TabIndex = 8;
             button2.Text = "Seçili Ürünü Sil";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(436, 159);
+            button3.BackColor = Color.Sienna;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(458, 191);
             button3.Name = "button3";
-            button3.Size = new Size(128, 29);
+            button3.Size = new Size(193, 29);
             button3.TabIndex = 9;
             button3.Text = "Fiyatı Güncelle";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
             Controls.Add(button3);
             Controls.Add(button2);
@@ -144,8 +205,11 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            DoubleBuffered = true;
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 162);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Bakery Management System";
+            Load += Form1_Load_1;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
